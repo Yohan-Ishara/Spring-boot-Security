2201bccd9c3ca09162d814f700db330bcb2bb2e1
Yohan-Ishara <yohanishara01@gmail.com>
2022-12-13 21:07:27 +0530

    add basic security

--- src/main/java/com/security/demo/springbootsecurity/config/ApplicationSecurityConfig.java
+++ src/main/java/com/security/demo/springbootsecurity/config/ApplicationSecurityConfig.java
@@ -1,21 +1,25 @@
 package com.security.demo.springbootsecurity.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @EnableWebSecurity
-public class ApplicationSecurityConfig  {
+@Configuration
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin();
-        http.httpBasic();
-        return http.build();
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
-
 }
