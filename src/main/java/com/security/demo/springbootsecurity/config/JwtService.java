bb44deebe3b42e44761cdeb122a220e4ecaf5a92
Yohan-Ishara <yohanishara01@gmail.com>
2023-01-06 10:22:13 +0530

    implement token claims

---
+++ src/main/java/com/security/demo/springbootsecurity/config/JwtService.java
@@ -0,0 +1,40 @@
+package com.security.demo.springbootsecurity.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private final static String SECRET_KEY = "2B4B6250655367566B5970337336763979244226452948404D635166546A576D";
+
+    public String extractUsername(String token) {
+        return extractClaims(token,Claims::getSubject);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
